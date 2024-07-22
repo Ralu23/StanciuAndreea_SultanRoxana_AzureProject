aa89217b4d317e53ec21675023a102667a4229b4
RoxanaSultan <roxana.sultan@student.unitbv.ro>
2024-07-22 16:20:13 +0300

    Changed code

--- null_resource.tf
+++ null_resource.tf
@@ -1,17 +1,17 @@
 resource "null_resource" "ping_test" {
-  count = var.vm_count - 1
+  count = var.vm_count
 
   provisioner "remote-exec" {
     connection {
       type        = "ssh"
-      host        = element(azurerm_network_interface.rg.*.private_ip_address, count.index)
+      host        = azurerm_linux_virtual_machine.rg[count.index].private_ip_address
       user        = "adminuser"
       private_key = file("~/.ssh/id_rsa")
       timeout     = "5m"
     }
 
     inline = [
-      "ping -c 4 ${element(azurerm_network_interface.rg.*.private_ip_address, count.index + 1)}"
+      "ping -c 4 ${azurerm_linux_virtual_machine.rg[count.index + 1].private_ip_address}"
     ]
   }
 
